d6c33d7b1a95b8c4ffdbfd7e257164eb23eba5bd
MasWag <tsugarutamenobu@gmail.com>
2013-10-10 14:38:36 +0900

    tass

---
+++ parse.h
@@ -0,0 +1,17 @@
+/*!
+  @file parse.h
+  @author Masaki Waga <tsugarutamenobu@gmail.com>
+  @date 2013/10/10
+  
+  @brief Schemeのパーサー
+*/
+
+#pragma once
+
+#include "types.h"
+
+atom_t parseAtom(void)
+{
+  
+
+}
